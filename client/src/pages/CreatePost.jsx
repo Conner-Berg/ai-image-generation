9b6cc92ef477b141a3572d76b860a37a2218974b
Conner-Berg <connerwberg@gmail.com>
2023-04-30 19:28:14 -0400

    Add handleSubmit logic for form submission

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -45,8 +45,35 @@ const CreatePost = () => {
 		}
 	};
 
-	const handleSubmit = () => {
-		// Create backend route to save post
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (form.prompt && form.photo) {
+			setLoading(true);
+
+			try {
+				const response = await fetch(
+					"http://localhost:8080/api/v1/post",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(form),
+					}
+				);
+
+				await response.json();
+				navigate("/");
+			} catch (error) {
+				alert(error);
+				console.log(error);
+			} finally {
+				setLoading(false);
+			}
+		} else {
+			alert("Please enter a prompt and generate an image");
+		}
 	};
 
 	const handleChange = (e) => {
